6de7b2d0520c20c6e2b038b0213cf0f7c2421ecb
Niels Fallenbeck <niels.fallenbeck@lrz.de>
2024-02-19 14:45:23 +0100

    Highlight and enforce max text width of 80 cols

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Highlight (and enforce) max text width of 80 characters
+vim.opt.colorcolumn = "80"
+vim.opt.textwidth = 80
