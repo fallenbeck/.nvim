18a88482865753aac4580a8f0038744901685d9b
Niels Fallenbeck <niels@fallenbeck.com>
2024-05-10 08:35:02 +0200

    Add vim options (for documentation purposes)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,11 @@
 -- Highlight (and enforce) max text width of 80 characters
 vim.opt.colorcolumn = "80"
 vim.opt.textwidth = 80
+
+-- Get indentation right!
+-- NOTE: In LazyVim this is set in the stylua file. However, I keep it here for
+-- documentation purposes
+-- vim.opt.tabstop = 4 -- A Tab character looks like 4 spaces
+-- vim.opt.expandtab = true -- Pressting Tab key will insert spaces instead of Tabs
+-- vim.opt.softtabstop = 4 -- Number of spaces inserted instead of Tab characters
+-- vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
