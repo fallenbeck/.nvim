577aa4ac02bd30d045584cbfa3911f0da9afdee8
Niels Fallenbeck <niels@fallenbeck.com>
2024-11-01 12:01:07 +0100

    Add autocmd to ensure that mason-update-all is called synced during headless update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- RubixDev/mason-update-all
+-- required when using headless updates
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MasonUpdateAllComplete",
+  callback = function()
+    print("mason-update-all has finished")
+  end,
+})
